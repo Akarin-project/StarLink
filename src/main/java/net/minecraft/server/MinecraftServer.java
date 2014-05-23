473dc10c63588c538e965c7841f8707f5cb6cf12
drXor <mcyoungsota@gmail.com>
2014-05-23 18:05:10 -0400

    Configurable save-on-stop-only for UserCache

--- src/main/java/net/minecraft/server/MinecraftServer.java
+++ src/main/java/net/minecraft/server/MinecraftServer.java
@@ -738,6 +738,12 @@ public abstract class MinecraftServer extends IAsyncTaskHandlerReentrant<TickTas
             this.snooper.e();
         }
 
+        // Spigot start
+        if (org.spigotmc.SpigotConfig.saveUserCacheOnStopOnly) {
+            LOGGER.info("Saving usercache.json");
+            this.getUserCache().c();
+        }
+        // Spigot end
     }
 
     public String getServerIp() {
